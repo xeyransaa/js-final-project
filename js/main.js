a166627801fa992d76a9636031b370025510a52a
xeyransa <kheyransa.aliyeva@gmail.com>
2022-01-05 16:29:48 +0400

    addition

--- js/main.js
+++ js/main.js
@@ -40,3 +40,19 @@ for (let i = 0; i < slideBtns.length; i++) {
 setInterval(() => {
     slider()
 }, 4000);
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:5,
+    loop:true,
+    margin:30,
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
